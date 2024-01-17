29214c00463eba3f8b6758ff49333644be7ea288
Maxence Chevee <max.rothen@hotmail.fr>
2024-01-17 15:21:16 +0100

    fix migrate, seed and schema

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -1,12 +1,62 @@
+DROP TABLE IF EXISTS shopping_cart;
+DROP TABLE IF EXISTS mascara;
+DROP TABLE IF EXISTS foundation;
+DROP TABLE IF EXISTS lipstick;
 DROP TABLE IF EXISTS users;
-CREATE TABLE users (
-id INT NOT NULL AUTO_INCREMENT,
-firstname VARCHAR(30) DEFAULT NULL,
-lastname VARCHAR(30) DEFAULT NULL,
-pseudoname VARCHAR(30)  NOT NULL,
-mail VARCHAR(90),
-birthdate DATE NOT NULL,
-logdate DATE NOT NULL,
-password VARCHAR(200),
-PRIMARY KEY (`id`)
+
+CREATE TABLE mascara (
+  id INT NOT NULL AUTO_INCREMENT,
+  code_color VARCHAR(30) NOT NULL,
+  name_color VARCHAR(30) NOT NULL,
+  quantity INT NOT NULL,
+  effect VARCHAR(30) NOT NULL,
+  price DECIMAL(10, 2) NOT NULL,
+  PRIMARY KEY (id)
 );
+
+CREATE TABLE foundation (
+  id INT NOT NULL AUTO_INCREMENT,
+  code_color VARCHAR(30) NOT NULL,
+  name_color VARCHAR(30) NOT NULL,
+  quantity INT NOT NULL,
+  aspect VARCHAR(30) NOT NULL,
+  price DECIMAL(10, 2) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE lipstick (
+  id INT NOT NULL AUTO_INCREMENT,
+  code_color VARCHAR(30) NOT NULL,
+  name_color VARCHAR(30) NOT NULL,
+  weight INT NOT NULL,
+  aspect VARCHAR(30) NOT NULL,
+  price DECIMAL(10, 2) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE users (
+  id INT NOT NULL AUTO_INCREMENT,
+  firstname VARCHAR(30) DEFAULT NULL,
+  lastname VARCHAR(30) DEFAULT NULL,
+  pseudoname VARCHAR(30) NOT NULL,
+  mail VARCHAR(90),
+  birthdate DATE NOT NULL,
+  logdate DATE NOT NULL,
+  password VARCHAR(200),
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE shopping_cart (
+  id INT NOT NULL AUTO_INCREMENT,
+  user_id INT NOT NULL,
+  item_id INT NOT NULL,
+  item_type VARCHAR(30) NOT NULL,
+  quantity INT,
+  PRIMARY KEY (id),
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  CHECK (item_type IN ('mascara', 'foundation', 'lipstick'))
+);
+
+ALTER TABLE shopping_cart ADD CONSTRAINT fk_shopping_cart_mascara FOREIGN KEY (item_id) REFERENCES mascara(id) ON UPDATE CASCADE ON DELETE CASCADE;
+ALTER TABLE shopping_cart ADD CONSTRAINT fk_shopping_cart_foundation FOREIGN KEY (item_id) REFERENCES foundation(id) ON UPDATE CASCADE ON DELETE CASCADE;
+ALTER TABLE shopping_cart ADD CONSTRAINT fk_shopping_cart_lipstick FOREIGN KEY (item_id) REFERENCES lipstick(id) ON UPDATE CASCADE ON DELETE CASCADE;
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import NavBar from "../components/NavBar";
+import "../styles/Home.scss";
+
+function Home() {
+  return (
+    <>
+      <NavBar />
+      <div className="HomeComponent">
+        <h1>Home</h1>
+
+        <p>
+          Découvrez toutes nos gammes de fond de teint, et testez sur votre peau
+          celui qui vous correspond le mieux grâce à notre simulateur.
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default Home;
